7511dad6e23a3fbe80183cd694a1c0b97b9ad5c9
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2024-01-23 17:11:04 -0500

    products section new style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,7 +100,7 @@ export default function Home() {
       <section className="relative bg-gray-50 md:py-24 py-16" id="services">
         <div className="container relative">
           <div className="grid grid-cols-1 pb-8 text-center">
-            <h6 className="text-indigo-600 text-sm font-bold uppercase mb-2">Servicios</h6>
+            <h6 className="text-cyan-500 text-sm font-bold uppercase mb-2">Servicios</h6>
             <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Nuestros servicios</h3>
             <p className="text-slate-400 max-w-xl mx-auto">Somos una empresa dedicada a la consultoría ambiental, contamos con una gran trayectoria en el sector ofreciendo servicios de calidad.</p>
           </div>
@@ -110,16 +110,16 @@ export default function Home() {
               let Icons = item.icon
               return(
                 <div key={index} className="p-6 hover:shadow-xl hover:shadow-gray-100 dark:hover:shadow-gray-800 transition duration-500 rounded-2xl mt-6">
-                  <div className="w-20 h-20 bg-indigo-600/5 text-indigo-600 rounded-xl text-3xl flex align-middle justify-center items-center shadow-sm dark:shadow-gray-800">
+                  <div className="w-20 h-20 bg-indigo-600/5 text-cyan-500 rounded-xl text-3xl flex align-middle justify-center items-center shadow-sm dark:shadow-gray-800">
                     <Icons className='w-7 h-7'/>
                   </div>
         
                   <div className="content mt-7">
-                    <Link to="/page-services" className="title h5 text-lg font-medium hover:text-indigo-600">{item.title}</Link>
+                    <Link to="/page-services" className="title h5 text-lg font-medium">{item.title}</Link>
                     <p className="text-slate-400 mt-3">{item.desc}</p>
         
                     <div className="mt-5">
-                      <Link to="/page-services" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500">Leer más <FaArrowRight className="ms-2 text-[10px]"/></Link>
+                      <Link to="/page-services" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-cyan-500 hover:text-green-ch-700 after:bg-green-600 duration-500">Leer más <FaArrowRight className="ms-2 text-[10px]"/></Link>
                     </div>
                   </div>
                 </div>
