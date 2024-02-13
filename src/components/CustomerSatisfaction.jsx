eb03fc9e8f1c186462fc9f90ffef94f7964c58e0
santiagospinai7 <santiagospidrobo15@gmail.com>
2024-02-12 19:24:10 -0500

    client satisfaction graph new style

--- src/components/CustomerSatisfaction.jsx
+++ src/components/CustomerSatisfaction.jsx
@@ -32,15 +32,15 @@ export default function CustomerSatisfaction({ title }) {
             Nuestros clientes son muy importantes para nosotros, por eso siempre buscamos brindarles el mejor servicio. Estas son nuestras estadísticas de satisfacción según nuestros clientes:
           </p>
         </div>
-        <div className="md:w-1/2 flex md:flex-row flex-col items-center">
+        <div className="md:w-1/2 flex md:flex-row flex-col items-center mt-10">
           <div className="md:max-w-xs w-full">
             <PieChart chartData={chartData} />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-wrap pt-5 gap-3 items-center">
             {clientSatisfactionData.map((item) => (
-              <div key={item.id} className="flex items-center gap-2 my-1">
+              <div key={item.id} className="flex gap-2 my-1 m-auto">
                 <span className="block w-4 h-4" style={{ backgroundColor: item.color }}></span>
-                <span className="text-sm">{item.label}</span>
+                <span className="text-sm px-1">{item.label}</span>
               </div>
             ))}
           </div>
